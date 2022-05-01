593d397e2b83b58c25d8957a0cacecc24ed1a004
Linda Lee <leeld1012@gmail.com>
2022-05-01 22:58:25 +0900

    stop() start() 추가

--- 02-project/js/swiper.js
+++ 02-project/js/swiper.js
@@ -1,6 +1,6 @@
 const swiper1 = new Swiper(".mainviewSwiper", {
   autoplay: {
-    delay: 5000,
+    delay: 5000
   },
   slidesPerView: 1,
   loop: true,
@@ -17,7 +17,6 @@ const swiper1 = new Swiper(".mainviewSwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-
 
 const swiper2 = new Swiper(".videoSwiper", {
   direction: "horizontal",
@@ -56,4 +55,34 @@ const swiper3 = new Swiper(".newsSwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+// stop() start()------------------------
+// mainview swiper---------------
+const pauseBtn1 = $('#mainview .swiper-pause');
+const playBtn1 = $('#mainview .swiper-play');
+
+pauseBtn1.on('click', function () {
+  $(this).css({'display' : 'none'});
+  playBtn1.css({'display' : 'block'});
+  swiper1.autoplay.stop();
+});
+playBtn1.on('click', function () {
+  $(this).css({'display' : 'none'});
+  pauseBtn1.css({'display' : 'block'});
+  swiper1.autoplay.start();
+});
+
+// news swiper---------------------
+const pauseBtn3 = $('#news .swiper-pause');
+const playBtn3 = $('#news .swiper-play');
+pauseBtn3.on('click', function () {
+  $(this).css({'display' : 'none'});
+  playBtn3.css({'display' : 'block'});
+  swiper3.autoplay.stop();
+});
+playBtn3.on('click', function () {
+  $(this).css({'display' : 'none'});
+  pauseBtn3.css({'display' : 'block'});
+  swiper3.autoplay.start();
 });
